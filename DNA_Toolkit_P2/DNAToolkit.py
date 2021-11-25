5898699cef19462122f8688142e550b3066324b1
Joy <mgsolo@163.com>
2021-11-25 14:26:35 +0800

    练习

--- DNA_Toolkit_P2/DNAToolkit.py
+++ DNA_Toolkit_P2/DNAToolkit.py
@@ -24,3 +24,15 @@ def transcription(seq):
 def reverse_complement(seq):
     """Swapping adenine with thymine and quanine with cytosine. Reversing newly generated string"""
     return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])
+
+def gc_content(seq):
+    """GC Content in a DNA/RNA sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_sebsec(seq, k=20):
+    """GC content in a DNA/RNA sub-sequence lenth K, k=20 by default"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i:i + k]
+        res.append(gc_content(subseq))
+    return res
--- DNA_Toolkit_P2/main.py
+++ DNA_Toolkit_P2/main.py
@@ -16,3 +16,7 @@ print(f'[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}\n')
 print(f"[4] + DNA String + Reverse Complement:\n5' {colored(DNAStr)} 3'")
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
 print(f"3' {colored(reverse_complement(DNAStr))} 5'\n")
+
+print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
+print(f'[6] + GC Content in Subsequence k=5: '
+      f'{gc_content_sebsec(DNAStr, k = 5)}\n')
